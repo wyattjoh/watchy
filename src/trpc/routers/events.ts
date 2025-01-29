a5659d622b7749583eaf1f9362ffc1c6c54a72e4
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-01-28 18:40:48 -0700

    feat: added debounce back to events

--- src/trpc/routers/events.ts
+++ src/trpc/routers/events.ts
@@ -1,24 +1,53 @@
 import { getEvents } from "@/lib/docker";
+import { setTimeout } from "node:timers/promises";
 import { baseProcedure } from "../init";
 
 export const eventsRouter = baseProcedure.subscription(async function* (opts) {
   const stream = await getEvents(opts.signal);
   const reader = stream.getReader();
 
+  let promise: Promise<ReadableStreamReadResult<Uint8Array>> | null = null;
+  let result: ReadableStreamReadResult<Uint8Array> | null;
+  let hadNewEvents = false;
+
   try {
     while (!opts.signal?.aborted) {
-      const { done } = await reader.read();
-      if (done) {
-        break;
+      // Read the next event (or the previous event if it's still pending).
+      promise = promise ?? reader.read();
+      result = await promise;
+      promise = null;
+      if (result.done) break;
+
+      // Emit the event signal to the client.
+      yield true;
+
+      // Wait for 500ms before yielding again.
+      await setTimeout(500);
+
+      // Drain all pending events for 500ms.
+      while (!opts.signal?.aborted) {
+        promise = reader.read();
+        result = await Promise.race([
+          promise,
+          setTimeout(500).then(() => null),
+        ]);
+
+        // If the timer expired, break out of draining.
+        if (result === null) break;
+
+        // Otherwise record that we had new event or that we're done.
+        promise = null;
+        if (result.done) return;
+        hadNewEvents = true;
       }
 
-      yield true;
+      // If we had new events, emit the event signal to the client.
+      if (hadNewEvents) yield true;
     }
   } catch (error) {
     if (error instanceof Error && error.name === "ResponseAborted") {
       return;
     }
-
     throw error;
   } finally {
     reader.releaseLock();
